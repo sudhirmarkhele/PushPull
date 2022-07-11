b8ad319cfb6be6a073a301c42f0c4885a38817e2
sudhir markhele <sudhirmarkhele@gmail.com>
2022-07-11 13:48:23 +0530

    pushpullpractice

---
+++ AutomationTesting/src/com/xyz/practice/BrowserTesting.java
@@ -0,0 +1,26 @@
+package com.xyz.practice;
+
+
+
+public class BrowserTesting {
+
+	public static void main(String[] args) {
+		
+				// step-1 register the driver
+				// webdriver.chrome.driver =
+				
+				System.setProperty("webdriver.chrome.driver", "E:\\23 feb batch workspace\\AutomationTesting\\Resources\\chromedriver.exe");
+				
+		        // step-2
+				// we have to create object of chrome
+				
+				ChromeDriver obj = new ChromeDriver ();
+				 // step 3
+				// enter url
+				obj.get("https://demoqa.com/text-box");
+			}
+
+		
+	}
+
+
---
+++ AutomationTesting/src/module-info.java
@@ -0,0 +1,3 @@
+module AutomationTesting {
+	requires okio;
+}
